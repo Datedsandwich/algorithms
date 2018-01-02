6dba0314e9e9627ec65f8640b38eb5e44ba880ea
Luke Robinson <robinsonlm@live.co.uk>
2018-01-02 15:19:06 +0000

    Implementation to determine if a string is a palindrome

---
+++ src/algorithms/isPalindrome/__test__/index.test.js
@@ -0,0 +1,18 @@
+import isPalindrome from '../index';
+
+describe('Is Palindrome', () => {
+    it('should return true if input is a palindrome with no spaces or punctuation', () => {
+        expect(isPalindrome('anna')).toBe(true);
+    })
+    it('should return true if input is a palindrome with spaces and no punctuation', () => {
+        expect(isPalindrome('dennis sinned')).toBe(true);
+    });
+
+    it('should return true if input is a palindrome with punctuation', () => {
+        expect(isPalindrome('A car, a man, a maraca')).toBe(true);
+    });
+
+    it('should return false if input is not a palindrome', () => {
+        expect(isPalindrome('A car, a man, several maracas')).toBe(false);
+    });
+});
