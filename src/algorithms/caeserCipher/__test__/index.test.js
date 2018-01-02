b1820826d08d33f10e22bf58ae8d669c76d51b77
Luke Robinson <robinsonlm@live.co.uk>
2018-01-02 16:12:39 +0000

    Identified some additional test cases for the Caeser Cipher

--- src/algorithms/caeserCipher/__test__/index.test.js
+++ src/algorithms/caeserCipher/__test__/index.test.js
@@ -8,4 +8,16 @@ describe('Caeser Cipher', () => {
     it('should be able to shift characters backwards if a negative number is passed in', () => {
         expect(caeserCipher('c', -2)).toEqual('a');
     });
+
+    it('should maintain letter case after shifting', () => {
+        expect(caeserCipher('AbA', 2)).toEqual('CdC');
+    })
+
+    it('should be able to handle numbers larger than 26', () => {
+        expect(caeserCipher('z', 27)).toEqual('a');
+    });
+
+    it('should be able to handle numbers much larger than 26', () => {
+        expect(caeserCipher('Javascript', -900)).toEqual('Tkfkcmbszd');
+    })
 });
