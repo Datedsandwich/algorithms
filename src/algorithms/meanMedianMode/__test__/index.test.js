97439fb522d266ae9b84418ecc1c979feb1d2c10
Luke Robinson <robinsonlm@live.co.uk>
2018-07-31 10:08:12 +0100

    Tests passing for Median

--- src/algorithms/meanMedianMode/__test__/index.test.js
+++ src/algorithms/meanMedianMode/__test__/index.test.js
@@ -13,6 +13,12 @@ describe("Mean Median Mode", () => {
     expect(median).toBe(30);
   });
 
+  it("should return an object with the median of the given set of numbers with an even number of values", () => {
+    const numbers = [10, 20, 30, 40, 50, 60];
+    const { median } = meanMedianMode(numbers);
+    expect(median).toBe(35);
+  });
+
   it("should return an object with the mode of the given set of numbers", () => {
     const numbers = [10, 20, 30, 40, 50, 10];
     const { mode } = meanMedianMode(numbers);
--- src/algorithms/meanMedianMode/index.js
+++ src/algorithms/meanMedianMode/index.js
@@ -1,12 +1,27 @@
 const getMean = array => array.reduce((a, b) => a + b, 0) / array.length;
 
-const getMedian = array => {};
+const getMedian = array => {
+  const sorted = [...array].sort((a, b) => a - b);
+
+  if (sorted.length % 2 != 0) {
+    return sorted[Math.floor(sorted.length / 2)];
+  } else {
+    const mid1 = sorted[sorted.length / 2 - 1];
+    const mid2 = sorted[sorted.length / 2];
+
+    const mid = [mid1, mid2];
+
+    return getMean(mid);
+  }
+};
 
 const getMode = array => {};
 
 const meanMedianMode = array => {
   return {
-    mean: getMean(array)
+    mean: getMean(array),
+    median: getMedian(array),
+    mode: getMode(array)
   };
 };
 
